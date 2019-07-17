eefe80769e0fd36f22b6854d16120e72718bc897
bootlegsorcery <offically_404@hotmail.com>
2019-07-17 13:08:46 +1000

    Fixed Index

--- src/awesome.py
+++ src/awesome.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/anime')
+def anime():
+    return render_template('anime.html', title='Anime')
+
 @app.route('/')
 def index():
-    return render_template('anime.html', title='Anime')
+    return render_template('index.html', title='Index')
