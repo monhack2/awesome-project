3ea9695b90fad716e3f9e24835986f8fc2d3f101
Tim Silvester <tim.silvester45@outlook.com>
2019-07-17 13:15:15 +1000

    Initial Commit for branch

--- src/awesome.py
+++ src/awesome.py
@@ -3,6 +3,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+class page:
+    def __init__(self, title, url):
+        self.title = title
+        self.url = url
+
+
 @app.route('/')
 def index():
-    return render_template('index.html', title='Index')
+    title = "Index"
+    page_list = [page("Index", "/"), page("Gud Page", "/gudPage")]
+    return render_template('index.html', **locals())
+
+
+@app.route('/gudPage')
+def gudPage():
+    title = "Gud Page"
+    page_list = [page("Index", "/"), page("Gud Page", "/gudPage")]
+    return render_template('index.html', **locals())
